8b9a81e80ad345d7ff09d44ca8fe11819ece5ffc
trungbx <trungbx@truesight.asia>
2024-07-17 16:05:46 +0700

    update component loading

---
+++ src/components/IconLoading/IconLoading.tsx
@@ -0,0 +1,39 @@
+import LoadingOutlined from "@ant-design/icons/LoadingOutlined";
+import { Spin } from "antd";
+import React from "react";
+
+export interface IconLoadingAction {
+  name?: string;
+  action?: any;
+}
+
+export interface IconLoadingProps {
+  /** Size of icon */
+  size?: number;
+  /** Color of icon */
+  color?: string;
+}
+function IconLoading(props: IconLoadingProps) {
+  const { size, color } = props;
+
+  return (
+    <Spin
+      indicator={
+        <LoadingOutlined
+          style={{
+            fontSize: size,
+            color: color,
+          }}
+          spin
+        />
+      }
+    />
+  );
+}
+
+IconLoading.defaultProps = {
+  size: 16,
+  color: "white",
+};
+
+export default IconLoading;
