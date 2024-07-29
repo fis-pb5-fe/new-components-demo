741227e6f7871b46b89f09213e38fa7f7d1de168
khangmeo123 <minhkhang231199@gmail.com>
2024-07-29 14:25:06 +0700

    add InputTag

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,3 +3,4 @@ export { default as Input } from "./Input/InputText";
 export { default as InputNumber } from "./Input/InputNumber";
 export { default as DatePicker } from "./Input/DatePicker";
 export { default as TextArea } from "./Input/TextArea";
+export { default as InputTag } from "./Input/InputTag";
