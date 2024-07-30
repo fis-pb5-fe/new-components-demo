cca5c49b971666a5247bfcf580ac97d7062c79cf
khangmeo123 <minhkhang231199@gmail.com>
2024-07-30 13:39:43 +0700

    add inputRange

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,3 +4,4 @@ export { default as InputNumber } from "./Input/InputNumber";
 export { default as DatePicker } from "./Input/DatePicker";
 export { default as TextArea } from "./Input/TextArea";
 export { default as InputTag } from "./Input/InputTag";
+export { default as InputRange } from "./Input/InputRange";
